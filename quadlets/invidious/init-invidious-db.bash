eef5d9b19a5efdbd11042e496a0a53b594bd54ca
redbeardymcgee <redbeardymcgee@proton.me>
2025-11-08 11:17:40 -0600

    fix(invidious): init db inside real container

--- quadlets/invidious/init-invidious-db.bash
+++ quadlets/invidious/init-invidious-db.bash
@@ -1,20 +1,20 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 set -eou pipefail
 
-## NOTE:
-## These should remain in sync with POSTGRES_USER && POSTGRES_DB in ./invidious.container
-pguser=${1:-kemal}
-pgdb=${2:-invidious}
+## NOTE: Depends on podman secret `invidious-db-pw`
+systemctl --user start invidious-db
 
-## NOTE:
-## `invidious-db` matches the volume name defined in ./invidious-db.volume
-podman volume create invidious-db
+podman exec invidious-db
+  sh -c '
+        apt-get update
+        apt-get install --assume-yes --no-install-recommends curl
 
-podman run \
-  --rm \
-  -v invidious-db:/var/lib/postgresql/data \
-  -e POSTGRES_USER=$pguser \
-  -e POSTGRES_DB=$pgdb \
-  --secret=invidious-db-pw,type=env,target=POSTGRES_PASSWORD \
-  docker.io/library/postgres:14 \
-  sh -c 'for initdb in channels videos channel_videos users session_ids nonces annotations playlists playlist_videos; do curl -s https://raw.githubusercontent.com/iv-org/invidious/refs/heads/master/config/sql/$initdb.sql | psql postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@localhost/$POSTGRES_DB'
+        for initdb in channels videos channel_videos users session_ids nonces annotations playlists playlist_videos
+        do
+          curl -s https://raw.githubusercontent.com/iv-org/invidious/refs/heads/master/config/sql/$initdb.sql | psql postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@invidious-db/$POSTGRES_DB
+        done
+        apt-get --assume-yes purge curl
+        '
+
+systemctl --user stop invidious-db
